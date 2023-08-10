48d6613ad6c4e805fe751130859b4b330770fc79
jonathanlunabiom <jonny-luna@hotmail.com>
2023-08-10 14:27:45 -0600

    added readme part

---
+++ generatedREADME.md
@@ -0,0 +1,41 @@
+# Movie night
+
+[asdsd](sdaas)
+
+## Table of Contents
+- [Description](#description)
+- [Instructions](#instructions)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Screenshot](#test)
+- [License](#license)
+- [¿Questions?](#questions)
+
+## Description <br/>
+
+sdsd
+
+## Instructions <br/>
+
+dsds
+
+## Usage <br/>
+
+dsdsd
+
+## Contribution <br/>
+
+dsada
+
+## Test <br/>
+
+![Movie night](https://www.creativefabrica.com/wp-content/uploads/2022/05/05/1651781912/Movie-Night-black-version-580x386.jpg)
+
+## License <br/>
+
+Movie night was release under the [asdsd](sdaas).
+
+## ¿Questions? <br/>
+
+Github: dsadas <br/>
+Email: dsadas
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {writeFile} = require('fs').promises;
 
-const READMEgenerate = ({title,description,instructions,usage,guidelines,test,license,github,email}) => 
+const generateREADME = ({title,description,instructions,usage,guidelines,test,license,licenseURL,github,email}) => 
 `# ${title}
 
-## Table of Contents
+[${license}](${licenseURL})
 
-- [Description](#Description)
-- [Instructions](#Instructions)
-- [Usage](#Usage)
-- [Contribution](#Contribution)
-- [Test](#Test)
-- [License](#License)
-- [Questions](#Questions)
+## Table of Contents
+- [Description](#description)
+- [Instructions](#instructions)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Screenshot](#test)
+- [License](#license)
+- [¿Questions?](#questions)
 
 ## Description <br/>
 
@@ -32,73 +34,80 @@ ${guidelines}
 
 ## Test <br/>
 
-${test}
+![${title}](${test})
 
 ## License <br/>
 
-${license}
+${title} was release under the [${license}](${licenseURL}).
 
-## Questions <br/>
+## ¿Questions? <br/>
 
 Github: ${github} <br/>
 Email: ${email}`
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter a title',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a description',
-    },
-    {
-        type: 'input',
-        name: 'instructions',
-        message: 'Set the intructions',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Set usage information',
-    },
-    {
-        type: 'input',
-        name: 'guidelines',
-        message: 'Enter the contribution guidelines',
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Set the test instructions',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'Write on the terminal one of the next license : [MIT License] [Unlicense] [ISC License]',
-    },
-    {
-        type: 'input',
-        name: 'licenseURL',
-        message: 'Enter the license url',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your github url',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email adress',
-    },
-])
-.then((answers) => {
-    const readmecontent = READMEgenerate(answers);
+const prompt = () =>{
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter a title',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description',
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'Set the intructions',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Set usage information',
+        },
+        {
+            type: 'input',
+            name: 'guidelines',
+            message: 'Enter the contribution guidelines',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter your image URL',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Write on the terminal one of the next license : [MIT License] [Unlicense] [ISC License]',
+            choices: ["yes", "no","no se"],
+        },
+        {
+            type: 'input',
+            name: 'licenseURL',
+            message: 'Enter the license url',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your github url',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email adress',
+        },
+    ])
+}
 
-    fs.writeFile('README.md', readmecontent, (e) => 
-    e ? console.log(e) : console.log("readme created!")
-    )
-})
+
+const init = () => {
+    prompt()
+    .then((answers) => writeFile('generatedREADME.md',generateREADME(answers)))
+    .then(() => console.log('Readme created!'))
+    .catch((e) => console.log(e));
+}
+
+init();
+
